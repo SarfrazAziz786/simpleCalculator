766cf7f8032c3603be0703ca07be848fa3c1da3f
unknown <sarfraz.aziz786@hotmail.com>
2024-03-22 21:41:29 +0500

    changes in text first and second number and added console

--- main.ts
+++ main.ts
@@ -7,8 +7,8 @@
 
 import inquirer from "inquirer";
 
-const answer = await inquirer.prompt([{message:"Enter first number", type: "number", name: "firstNumber"},
-{message: "Enter second number", type:"number", name:"secondNumber"},
+const answer = await inquirer.prompt([{message:"Enter your first number", type: "number", name: "firstNumber"},
+{message: "Enter your second number", type:"number", name:"secondNumber"},
 {message:"select one of the operator to perform action", type:"list", name:"operator", 
 choices: ["Addition", "Subtraction", "Multiplication", "Division"],
 },
@@ -32,6 +32,7 @@ if(answer.operator==="Addition") {
     
 };
 
+console.log("The End");
 
 
 
